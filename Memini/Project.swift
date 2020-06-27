02f95e91ff5b12de175bd24d9470133482b23143
Jess <jtrinh619@gmail.com>
2020-06-27 05:15:25 -0700

    create Project model

---
+++ Memini/Project.swift
@@ -0,0 +1,14 @@
+//
+//  Project.swift
+//  Memini
+//
+//  Created by Jessica Trinh on 6/27/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import Foundation
+
+struct Project {
+    let title: String
+    let subtitle: String
+}
