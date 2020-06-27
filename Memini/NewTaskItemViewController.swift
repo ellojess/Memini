69decf2a32124b847f4f715ca82bd582e38314cb
Jess <jtrinh619@gmail.com>
2020-06-26 23:56:39 -0700

    setup new task item page

--- Memini/NewTaskItemViewController.swift
+++ Memini/NewTaskItemViewController.swift
@@ -10,12 +10,89 @@ import Foundation
 import UIKit
 
 class NewTaskItemViewController: UIViewController {
+    
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 12
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+    
+//    let titleLabel: UILabel = {
+//        let titleLabel = UILabel()
+//        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+//        titleLabel.font = UIFont(name: "AvenirNextCondensed-DemiBold", size: 18)
+//        titleLabel.textColor = .black
+//        titleLabel.text = "Title"
+//        return titleLabel
+//    }()
+
+    let titleSection: UITextField = {
+        let textField = UITextField()
+        let bottomLine = UIView()
+        let titleLabel = UILabel()
+        
+        textField.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.font = UIFont(name: "AvenirNextCondensed-DemiBold", size: 18)
+        titleLabel.bottomAnchor.constraint(equalTo: textField.topAnchor, constant: 5).isActive = true
+        titleLabel.textColor = .black
+        titleLabel.text = "Title"
+        
+        textField.attributedPlaceholder = NSAttributedString(string: "e.g. Read 2 chapters of The Illiad",
+        attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        textField.font = UIFont(name: "AvenirNextCondensed-DemiBold", size: 18)
+        textField.textColor = .white
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.addSubview(bottomLine)
+        
+        bottomLine.translatesAutoresizingMaskIntoConstraints = false
+        bottomLine.backgroundColor = .white
+        bottomLine.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: -2).isActive = true
+        bottomLine.leadingAnchor.constraint(equalTo: textField.leadingAnchor).isActive = true
+        bottomLine.trailingAnchor.constraint(equalTo: textField.trailingAnchor).isActive = true
+        bottomLine.heightAnchor.constraint(equalToConstant: 1.0).isActive = true
+        return textField
+    }()
+    
+    
+    let dateSection: UITextField = {
+        let textField = UITextField()
+        let bottomLine = UIView()
+        let completeByLabel = UILabel()
+        
+        textField.addSubview(completeByLabel)
+        completeByLabel.translatesAutoresizingMaskIntoConstraints = false
+        completeByLabel.font = UIFont(name: "AvenirNextCondensed-DemiBold", size: 18)
+        completeByLabel.textColor = .black
+        completeByLabel.bottomAnchor.constraint(equalTo: textField.topAnchor, constant: 5).isActive = true
+
+        completeByLabel.text = "Complete By Date"
+        
+        textField.attributedPlaceholder = NSAttributedString(string: "mm/dd/yyyy",
+        attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        textField.font = UIFont(name: "AvenirNextCondensed-DemiBold", size: 18)
+        textField.textColor = .white
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.addSubview(bottomLine)
+        
+        bottomLine.translatesAutoresizingMaskIntoConstraints = false
+        bottomLine.backgroundColor = .white
+        bottomLine.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: -2).isActive = true
+        bottomLine.leadingAnchor.constraint(equalTo: textField.leadingAnchor).isActive = true
+        bottomLine.trailingAnchor.constraint(equalTo: textField.trailingAnchor).isActive = true
+        bottomLine.heightAnchor.constraint(equalToConstant: 1.0).isActive = true
+        return textField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .green
         setupNavBar()
+        setupView()
     }
     
     func setupNavBar() {
@@ -25,12 +102,32 @@ class NewTaskItemViewController: UIViewController {
         
         let saveButton = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(self.saveTask))
         navigationItem.rightBarButtonItem = saveButton
-        
     }
     
     @objc func saveTask() {
         self.navigationController?.popViewController(animated: true)
     }
+    
+    func setupView() {
+    
+    view.addSubview(stackView)
+    stackView.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.7).isActive = true
+    stackView.heightAnchor.constraint(equalTo: view.layoutMarginsGuide.heightAnchor, multiplier: 0.43).isActive = true
+    stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
 
+    self.view.backgroundColor = UIColor(red:0.49, green:0.84, blue:0.87, alpha:1.0)
+    
+    stackView.addArrangedSubview(titleSection)
+    titleSection.widthAnchor.constraint(equalTo: stackView.widthAnchor).isActive = true
+    titleSection.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+    stackView.setCustomSpacing(20, after: titleSection)
+    
+    stackView.addArrangedSubview(dateSection)
+    dateSection.widthAnchor.constraint(equalTo: stackView.widthAnchor).isActive = true
+    dateSection.heightAnchor.constraint(equalToConstant: 70).isActive = true
+    
 
+    }
 }
